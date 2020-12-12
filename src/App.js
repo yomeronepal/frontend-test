be8a017025f2a5dd5136d7f36ad1b510aa9c6036
Rojesh Prajapati <yomeronepal7@gmail.com>
2020-12-12 15:58:04 +0545

    loading state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [todos,setTodos] = useState(['Talk dogs for walk','Talk dogs for walk','Let me sleep']);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Hello World </h1>
+        <input />
+        <button>Add Todo </button>
+
+        <ul> 
+          {todos.map(todo =>(
+              <li>{todo}</li>
+            ))
+          }
+        </ul>
+
     </div>
+
+    
   );
 }
 
